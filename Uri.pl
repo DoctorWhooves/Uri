7b03671041b1d5a0fcedf3d6f6fc931c3b6e174a
Doctor Whooves <DoctorWhooves@users.noreply.github.com>
2014-05-19 20:28:22 -0400

    Update and rename Uri to Uri.pl

--- Uri.pl
+++ Uri.pl
@@ -21,13 +21,19 @@ sub create_sentence {
 sub parse_command {
 	my $string = $_[0];
 	my $res;
+	my @matches;
 
-	if ($string =~ /date/) {
+	if ($string =~ /(what is).*(date)/) {
 		$res = `date +'%m/%d/%Y'`;
 		print "The current date is " . $res;
-	} elsif ($string =~ /time/) {
+	} elsif ($string =~ /(what is).*(time)/) {
 		$res = `date +'%r'`;
 		print "The current time is " . $res;
+	} elsif ($string =~ /(extract|untar)/) {
+		@matches = $string =~ /(untar|extract)\s(.*)/;
+		print "Extracting $matches[1] into current directory...\n";
+		$res = `tar -zxvf $matches[1]`;
+		print "Extraction complete!\n";
 	}
 }
 
